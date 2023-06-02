8541dfcf90cdb9b32e9516ed5756f1ffaecf7941
KOsay228 <135262622+KOsay228@users.noreply.github.com>
2023-06-02 16:27:15 +0700

    Update some_app.py

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -1,2 +1,12 @@
-print("hello world")
+import unittest
 
+def summa(a, b):
+return a * b
+
+class TestAddition(unittest.TestCase):
+def test_addition(self):
+result = summa(2, 3)
+self.assertEqual(result, -1)
+
+if __name__ == '__main__':
+unittest.main()
